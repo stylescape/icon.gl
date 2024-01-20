4f0ed1f28ca71596f674b6cd9bbb4e176ef17104
Lars van Vianen <lars@vianen.com>
2024-01-20 12:54:02 +0100

    v0.0.12

---
+++ dist/ts/icons/ui_square_create.ts
@@ -0,0 +1,1 @@
+export const icon_ui_square_create = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 864 864"><path d="M684 144H180c-19.88 0-36 16.12-36 36v504c0 19.88 16.12 36 36 36h504c19.88 0 36-16.12 36-36V180c0-19.88-16.12-36-36-36m0 540H180V180h504z"/><path d="M414 576h36V450h126v-36H450V288h-36v126H288v36h126z"/></svg>`;
---
+++ src/ts/icons/ui_square_fill_create.ts
@@ -0,0 +1,1 @@
+export const icon_ui_square_fill_create = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 864 864"><path d="M684 144H180c-19.88 0-36 16.12-36 36v504c0 19.88 16.12 36 36 36h504c19.88 0 36-16.12 36-36V180c0-19.88-16.12-36-36-36M576 450H450v126h-36V450H288v-36h126V288h36v126h126z"/></svg>`;
